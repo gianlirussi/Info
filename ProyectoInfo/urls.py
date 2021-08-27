17d52c50bb4b4e6f5de00a8edebd782e7a414adc
gianlirussi <lirussigianluca7292@gmail.com>
2021-08-27 11:54:27 -0300

    statics

--- ProyectoInfo/urls.py
+++ ProyectoInfo/urls.py
@@ -15,6 +15,9 @@ Including another URLconf
 """
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+from usuario.views import registro, login, logout, home
 from django.conf import settings
 from django.conf.urls.static import static
 
